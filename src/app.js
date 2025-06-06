543a462194d4c7a0409c2173fc89eb49429a69ca
Samarth Jadhav <samarth10jadhav@gmail.com>
2025-06-06 20:32:55 +0530

    practiced Routing

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require('express');
 
 const app = express(); // creating express js application 
 
-app.use('/',(req , res)=>{
-    res.send("Hello dashboard");
-})
+app.get("/user/:userid",(req , res)=>{
+    console.log(req.query); // dynamic data from url. 
+    // dynamic routes 
+    console.log(req.params); 
+    res.send({"firstname" : "Samarth","Lastname" : "Jadhav"});
+});
 
-app.use('/getdata',(req , res)=>{
-    res.send("data received");
-})
+app.post("/user",(req , res)=>{
+    //saving data to db
+    res.send("Data successfully saved in db");
+});
+
+// app.use('/',(req , res)=>{
+//     res.send("Hello dashboard");
+// })
 
 app.listen(3000 , ()=>{
     console.log("server is running on port 3000");  
