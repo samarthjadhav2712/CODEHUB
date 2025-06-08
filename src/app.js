f92acd05e9bb75ca9369639b0a5a8e16d643a0ad
Samarth Jadhav <samarth10jadhav@gmail.com>
2025-06-08 20:49:06 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -2,15 +2,33 @@ const express = require('express');
 const app = express(); // creating express js application 
 const {auth} = require('./middlewares/auth'); // importing auth middleware
 
-app.use('/Admin',auth);
-
-app.get('/Admin/getUserData' ,(req,res,auth)=>{
-    res.send("User data fetched successfully");
+// one way to handle errors is to use try-catch blocks in route handlers .
+app.get('/admin',(req , res)=>{
+    try{
+    console.log("Admin route accessed");
+    throw new Error("Simulated error for testing error handling");
+    res.send("Welcome to the admin page");
+    }
+    catch(err){
+        console.error("Error in admin route:", err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
-app.post('/Admin/DeleteUserData' ,(req,res,auth)=>{
-    res.send("User data deleted successfully");
+// another way to handle errors is to use middleware functions that catch errors and send a response.
+app.get('/admin',(req,res,next)=>{
+    console.log("Admin route accessed");
+    // Simulating an error
+    const error = new Error("Simulated error for testing error handling");
+    next(error); // Pass the error to the next middleware
 });
+
+app.use((err,req,res,next)=>{
+        // This middleware will catch any errors passed to next(err) in the route handlers.
+        console.error("An error occurred:", err);
+        res.status(500).send("Internal Server Error");
+});
+
 
 app.listen(3000 , ()=>{
     console.log("server is running on port 3000");  
