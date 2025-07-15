6170d5c31ed2a916613178eb849e7df062a65cb5
Samarth Jadhav <samarth10jadhav@gmail.com>
2025-07-16 02:30:42 +0530

    ref , populate get/user/connections API

--- src/Routes/userRoute.js
+++ src/Routes/userRoute.js
@@ -35,4 +35,38 @@ userRouter.get("/user/request/received" ,userAuth, async(req,res)=>{
     }
 });
 
-module.exports = userRouter;
+userRouter.get("/user/connections",userAuth , async(req,res)=>{
+    try{
+    const loggedInUser = req.user;
+    
+    // get the users where either fromuserid / touserid is loggedinUser & status is accepted .
+    const user = await ConnectionRequest.find({
+  $or: [
+    { fromUserId: loggedInUser._id, status: "accepted" },
+    { toUserID: loggedInUser._id, status: "accepted" }
+  ]
+})
+.populate("fromUserId", "firstName lastName")
+.populate("toUserID", "firstName lastName");
+
+    if(!user || user.length==0){
+        throw new Error("user not found");
+    }
+
+    //If you want your connections list to always show the other user in the connection, you need this check.
+    const connections = user.map(req =>{
+        if(req.fromUserId._id.toString() === loggedInUser._id.toString()){
+            return req.toUserID;
+        }
+        return req.fromUserId;
+    });
+
+    res.json({message : `all the connections of ${loggedInUser.firstName} are listed below : `,
+    data : connections});
+    }
+    catch(err){
+        return res.status(400).json({message : `Error : ${err.message}`});
+    }
+});
+
+module.exports = userRouter ;
